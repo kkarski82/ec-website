29241ca970eca52ef4d3c6092e252a278ab2d451
Krzysztof Karski <krzysztof.karski@gmail.com>
2015-01-12 16:12:16 +0100

    translations and basic styling

--- src/Vtes/Bundle/WebsiteBundle/Controller/DefaultController.php
+++ src/Vtes/Bundle/WebsiteBundle/Controller/DefaultController.php
@@ -3,10 +3,21 @@
 namespace Vtes\Bundle\WebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
+    {
+        $locale = $request->getLocale();
+        $router = $this->get('router');
+        $url = $router->generate('vtes_website_homepage', array('_locale' => $locale));
+
+        return new RedirectResponse($url);
+    }
+
+    public function homeAction()
     {
         return $this->render('VtesWebsiteBundle:Default:index.html.twig');
     }
